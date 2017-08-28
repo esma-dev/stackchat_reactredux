a0ef6c6265a6bd9c6dcc94085731b289920e3f16
Asmaa H Abbas <abbas.asmaa@gmail.com>
2017-08-28 12:49:49 -0400

    completed sections 1-3

--- client/components/ChannelList.js
+++ client/components/ChannelList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import store from '../store';
 
@@ -9,57 +10,37 @@ const GENERAL_CHANNEL = '/channels/2';
 const DOGS_CHANNEL = '/channels/3';
 const LUNCH_CHANNEL = '/channels/4';
 
-export default class ChannelList extends Component {
+function ChannelList(props) {
 
-  constructor () {
-    super();
-    this.state = store.getState();
-  }
-
-  componentDidMount () {
-    this.unsubscribe = store.subscribe(() => this.setState(store.getState()));
-  }
-
-  componentWillUnmount () {
-    this.unsubscribe();
-  }
-
-  render () {
-
-    const { messages } = this.state;
+  // render() {
 
     return (
       <ul>
-        <li>
-          <NavLink to={RANDOM_CHANNEL} activeClassName="active">
-            <span># really_random</span>
-            <span className="badge">{ messages.filter(message => message.channelId === 1).length }</span>
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to={GENERAL_CHANNEL} activeClassName="active">
-            <span># generally_speaking</span>
-            <span className="badge">{ messages.filter(message => message.channelId === 2).length }</span>
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to={DOGS_CHANNEL} activeClassName="active">
-            <span># dogs_of_fullstack</span>
-            <span className="badge">{ messages.filter(message => message.channelId === 3).length }</span>
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to={LUNCH_CHANNEL} activeClassName="active">
-            <span># lunch_planning</span>
-            <span className="badge">{ messages.filter(message => message.channelId === 4).length }</span>
-          </NavLink>
-        </li>
+        { props.channels.map( (channel) => {
+          return (
+            <li key={channel.id}>
+              <NavLink to={`/channels/${channel.id}`} activeClassName="active">
+                <span># {channel.name}</span>
+                <span className="badge">{ props.messages.filter(message => message.channelId === channel.id).length }</span>
+              </NavLink>
+            </li>
+          )
+        })}
         <li>
           <NavLink to="/new-channel">Create a channel...</NavLink>
         </li>
       </ul>
     );
-  }
+  // }
 }
 
+function mapStateToProps(state){
+  return {
+    channels: state.channels,
+    messages: state.messages
+  };
+};
+
 /** Write your `connect` component below! **/
+const ChannelListContainer = connect(mapStateToProps)(ChannelList);
+export default ChannelListContainer;
